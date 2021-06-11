f9407ae59d43894758f6f180f55736fc0d6bba4b
gkennedy74 <gkennedy@soriana.com>
2021-06-11 09:17:28 -0500

    Function added returns the product of p1 and p2

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -8,3 +8,7 @@ function returnSmallest(a, b) {
 }
 
 // Add your functions below this line :) ------------------------------------
+
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+}
